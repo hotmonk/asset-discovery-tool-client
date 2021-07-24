cb98758f02fceaebf75b71ad8ff46d59c4bb6a1b
gauravsharma21 <gauravsharma10167@gmail.com>
2021-07-25 01:28:12 +0530

    changes

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,6 +1,7 @@
 import {React, useEffect, useState } from 'react';
 import TableComponent from './TableComponent';
 import styled from 'styled-components'
+import IPAddresses from '../IPaddreses.json'; 
 
 const TableContainer = styled.div`
     margin-top: 50px;
@@ -10,7 +11,7 @@ const PageContainer = styled.div`
     display: flex;
     justify-content: space-around;
 `
-
+console.log(IPAddresses);
 function HomeScreen(){
     const [data, setdata] = useState({});
 
@@ -18,6 +19,7 @@ function HomeScreen(){
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(IPAddresses),
         };
         fetch('http://localhost:8000', requestOptions)
             .then(response => response.json())
--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import {Table} from 'react-bootstrap';
+import React from "react";
+import { Table } from "react-bootstrap";
+import moment from "moment";
 
-function TableComponent({tableData}){
-
-    const rows=Object.keys(tableData).map((x)=>{
-                return <tr>
-                    <td>{x}</td>
-                    <td>{x.mac_address}</td>
-                    <td>{x.os}</td>
-                </tr>
-            })
+function TableComponent({ tableData }) {
+  const defaultValue = "-";
+  const rows = Object.keys(tableData).map((x, index) => {
     return (
-        <>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                    <th>#</th>
-                    <th>IP Address</th>
-                    <th>MAC Address</th>
-                    <th>Operating System</th>
-                    <th>Last Seen</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </Table>
-        </>
-    )
+      <tr>
+        <td>{index + 1}</td>
+        <td>{x}</td>
+        <td>{tableData[x].mac_address || defaultValue}</td>
+        <td>{tableData[x].os || defaultValue}</td>
+        <td>
+          <span
+            style={{
+              display: "inline-block",
+              borderRadius: "50%",
+              height: "20px",
+              width: "20px",
+              backgroundColor: tableData[x].last_seen.status === 'online' ? '#0ffc03' : 'red',
+              marginRight: "10px",
+            }}
+          ></span>
+          {`${moment(tableData[x].last_seen.time).format('YYYY-MM-DD HH:mm:ss')}`}
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>IP Address</th>
+            <th>MAC Address</th>
+            <th>Operating System</th>
+            <th>Last Seen</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </>
+  );
 }
 
 export default TableComponent;
